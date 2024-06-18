67971ddd469797e4840baac1f82d56da1da44006
bogdanahaf <bogdanagaf@gmail.com>
2024-06-17 20:47:01 -0700

    Update request.py

--- request.py
+++ request.py
@@ -4,9 +4,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime, timedelta
+import os
 
 # Define your Telegram bot token and chat ID
-import os
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
@@ -25,7 +25,6 @@ def scrape_availability(url):
     driver.get(url)
 
     try:
-        # Example: Click on buttons to load availability information
         print("HELLO")
         button1 = WebDriverWait(driver, 30).until(
             EC.element_to_be_clickable((By.CLASS_NAME, 'rec-select'))
@@ -41,7 +40,6 @@ def scrape_availability(url):
         button2.click()
         print("Button2 clicked")
 
-        # Find the second element with the class name 'rec-input-radio'
         buttons = WebDriverWait(driver, 30).until(
             EC.presence_of_all_elements_located((By.CLASS_NAME, 'rec-input-radio'))
         )
@@ -54,14 +52,13 @@ def scrape_availability(url):
             print("Not enough elements found with the class 'rec-input-radio'")
             return
 
-        # Wait for the availability information to load after button clicks
         WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.XPATH, '//*[@id="per-availability-main"]/div/div[5]/div[3]/div[2]/div[2]/div/div[2]/div/button/span/span'))
         )
         print("Availability information loaded")
 
-        # Extract and print availability information
-        availability_element = driver.find_element(By.XPATH, '//*[@id="per-availability-main"]/div/div[5]/div[3]/div[2]/div[2]/div/div[2]/div/button/span/span')
+        # availability_element = driver.find_element(By.XPATH, '//*[@id="per-availability-main"]/div/div[5]/div[3]/div[2]/div[2]/div/div[2]/div/button/span/span')
+        availability_element = driver.find_element(By.XPATH, '//*[@id="pean')
         availability_text = availability_element.text.strip()
         print(f"Availability Information: {availability_text}")
 
@@ -70,7 +67,9 @@ def scrape_availability(url):
             send_telegram_message(message)
 
     except Exception as e:
-        print(f"Error occurred: {str(e)}")
+        error_message = f"Error occurred: {str(e)}"
+        print(error_message)
+        send_telegram_message(error_message)
 
     finally:
         driver.quit()
@@ -84,7 +83,6 @@ def check_availability_for_dates(start_date, end_date):
         scrape_availability(url)
         current_date += timedelta(days=1)
 
-# Example usage
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 start_date = datetime.strptime('2024-08-27', '%Y-%m-%d')
 end_date = datetime.strptime('2024-08-30', '%Y-%m-%d')
